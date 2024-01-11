151f89420256968540b1ddcffca1878f28be6785
Alex <alexhg@e.email>
2024-01-11 09:41:40 -0500

    Make train script modular

--- train.py
+++ train.py
@@ -25,48 +25,80 @@ def add_args(parser):
         type=str,
         help="Test CSV",
     )
+    parser.add_argument(
+        "--do_normalize",
+        action="store_true",
+        help="Normalize train and test data",
+    )
+    parser.add_argument(
+        "--do_print",
+        action="store_true",
+        help="Print information during execution",
+    )
     return parser
 
 
-def main(args):
-    # Read data
-    if args.train:
-        df_tr = pd.read_csv(args.train, index_col=0)
-    else:
-        print("Train data file missing")
-        return
-    if args.test:
-        df_tt = pd.read_csv(args.test, index_col=0)
-    else:
-        print("Test data file missing")
-        return
-    features = [f for f in df_tr.keys() if f not in ["lat", "lon", "time", "LABELS"]]
+def read_data(
+    train_path, test_path, cols_exclude=["lat", "lon", "time", "LABELS"], do_print=True
+):
+    df_tr = pd.read_csv(train_path, index_col=0)
+    df_tt = pd.read_csv(test_path, index_col=0)
+    features = [f for f in df_tr.keys() if f not in cols_exclude]
     x_tr = df_tr[features].values
     x_tt = df_tt[features].values
     y_tr = df_tr.LABELS.values
     y_tt = df_tt.LABELS.values
-    print(f"Number of features: {len(features)}")
-    print(f"Number of classes: {np.unique(y_tr).shape[0]}")
-    print(f"Train size: {x_tr.shape[0]}")
-    print(f"Test size: {x_tt.shape[0]}")
+    if do_print:
+        print(f"Number of features: {len(features)}")
+        print(f"Number of classes: {np.unique(y_tr).shape[0]}")
+        print(f"Train size: {x_tr.shape[0]}")
+        print(f"Test size: {x_tt.shape[0]}")
+    return x_tr, x_tt, y_tr, y_tt
 
-    # Normalize
+
+def normalize(x_tr, x_tt):
     mean_tr = np.mean(x_tr, axis=0)
-    mean_tr = mean_tr[np.newaxis, :]
     std_tr = np.std(x_tr, axis=0)
-    std_tr = std_tr[np.newaxis, :]
     x_tr = (x_tr - mean_tr) / std_tr
     x_tt = (x_tt - mean_tr) / std_tr
+    return x_tr, x_tt
+
+
+def logistic_regression_train(x_tr, y_tr, n_iters=500, seed=0):
+    return LogisticRegression(max_iter=n_iters, random_state=seed).fit(x_tr, y_tr)
+
+
+def most_frequent_train(x_tr, y_tr):
+    return DummyClassifier(strategy="most_frequent").fit(x_tr, y_tr)
+
+
+def evaluate_accuracy(model, x, y):
+    return model.score(x, y)
+
+
+def main(args):
+    # Read data
+    if not args.train:
+        print("Train data file missing")
+        return
+    if not args.test:
+        print("Test data file missing")
+        return
+    x_tr, x_tt, y_tr, y_tt = read_data(args.train, args.test, do_print=args.do_print)
+
+    # Normalize
+    if args.do_normalize:
+        x_tr, x_tt = normalize(x_tr, x_tt)
 
     # Train models
-    baseline = DummyClassifier(strategy="most_frequent").fit(x_tr, y_tr)
-    logreg = LogisticRegression(random_state=0, max_iter=500).fit(x_tr, y_tr)
+    baseline = most_frequent_train(x_tr, y_tr)
+    logreg = logistic_regression_train(x_tr, y_tr, n_iters=500, seed=0)
 
     # Evaluation
-    baseline_acc_tr = baseline.score(x_tr, y_tr)
-    baseline_acc_tt = baseline.score(x_tt, y_tt)
-    logreg_acc_tt = logreg.score(x_tt, y_tt)
-    logreg_acc_tr = logreg.score(x_tr, y_tr)
+    baseline_acc_tr = evaluate_accuracy(baseline, x_tr, y_tr)
+    baseline_acc_tt = evaluate_accuracy(baseline, x_tt, y_tt)
+    logreg_acc_tt = evaluate_accuracy(logreg, x_tt, y_tt)
+    logreg_acc_tr = evaluate_accuracy(logreg, x_tr, y_tr)
     logreg_pred_tt = logreg.predict(x_tt)
     logreg_acc_tt_aux = np.mean(logreg_pred_tt == y_tt)
     assert np.isclose(logreg_acc_tt, logreg_acc_tt_aux)
