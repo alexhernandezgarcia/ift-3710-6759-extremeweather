65364a71fa82330f367e7e90666c69efc6e16645
Alex <alexhg@e.email>
2024-01-23 09:32:00 -0500

    Increase functionality of script: val/test splits, regularisation, etc.

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 def add_args(parser):
@@ -15,20 +16,44 @@ def add_args(parser):
     """
     parser.add_argument(
         "--train",
-        default=None,
+        default="data/train.csv",
         type=str,
         help="Train CSV",
     )
     parser.add_argument(
         "--test",
-        default=None,
+        default="data/test.csv",
         type=str,
         help="Test CSV",
+    )
+    parser.add_argument(
+        "--pct_val",
+        default=0.0,
+        type=float,
+        help="Percentage of the train set for the validation set.",
+    )
+    parser.add_argument(
+        "--pct_test",
+        default=0.0,
+        type=float,
+        help="Percentage of the train set for the test set. If larger than 0, the "
+        "original test set is reserved (ignored).",
     )
     parser.add_argument(
         "--do_normalize",
         action="store_true",
         help="Normalize train and test data",
+    )
+    parser.add_argument(
+        "--seed",
+        default=0,
+        type=int,
+        help="Random seed",
+    )
+    parser.add_argument(
+        "--do_hp_validation",
+        action="store_true",
+        help="Do hyper-parameter optimisation with the validation set",
     )
     parser.add_argument(
         "--do_print",
@@ -38,9 +63,7 @@ def add_args(parser):
     return parser
 
 
-def read_data(
-    train_path, test_path, cols_exclude=["lat", "lon", "time", "LABELS"], do_print=True
-):
+def read_data(train_path, test_path, cols_exclude=["lat", "lon", "time", "LABELS"]):
     df_tr = pd.read_csv(train_path, index_col=0)
     df_tt = pd.read_csv(test_path, index_col=0)
     features = [f for f in df_tr.keys() if f not in cols_exclude]
@@ -48,24 +71,56 @@ def read_data(
     x_tt = df_tt[features].values
     y_tr = df_tr.LABELS.values
     y_tt = df_tt.LABELS.values
-    if do_print:
-        print(f"Number of features: {len(features)}")
-        print(f"Number of classes: {np.unique(y_tr).shape[0]}")
-        print(f"Train size: {x_tr.shape[0]}")
-        print(f"Test size: {x_tt.shape[0]}")
     return x_tr, x_tt, y_tr, y_tt
 
 
-def normalize(x_tr, x_tt):
+def split(x_tr, y_tr, pct_val, pct_tt, seed=0):
+    n_val = int(pct_val * len(y_tr))
+    n_tt = int(pct_tt * len(y_tr))
+    # Validation split
+    if n_val > 0:
+        x_tr, x_val, y_tr, y_val = train_test_split(
+            x_tr, y_tr, test_size=n_val, shuffle=True, stratify=y_tr
+        )
+    else:
+        x_val = []
+        y_val = []
+    # Test split
+    if n_tt > 0:
+        x_tr, x_tt, y_tr, y_tt = train_test_split(
+            x_tr, y_tr, test_size=n_tt, shuffle=True, stratify=y_tr
+        )
+    else:
+        x_tt = []
+        y_tt = []
+    return x_tr, x_val, x_tt, y_tr, y_val, y_tt
+
+
+def normalize(x_tr, x_val, x_tt):
     mean_tr = np.mean(x_tr, axis=0)
     std_tr = np.std(x_tr, axis=0)
     x_tr = (x_tr - mean_tr) / std_tr
-    x_tt = (x_tt - mean_tr) / std_tr
-    return x_tr, x_tt
+    if len(x_val) > 0:
+        x_val = (x_val - mean_tr) / std_tr
+    if len(x_tt) > 0:
+        x_tt = (x_tt - mean_tr) / std_tr
+    return x_tr, x_val, x_tt
 
 
 def logistic_regression_train(x_tr, y_tr, n_iters=500, seed=0):
     return LogisticRegression(max_iter=n_iters, random_state=seed).fit(x_tr, y_tr)
+
+
+def logistic_regression_validate_reg_train(
+    x_tr, y_tr, x_val, y_val, c_values, n_iters=500, seed=0
+):
+    models = []
+    for c in c_values:
+        models.append(
+            LogisticRegression(C=c, max_iter=n_iters, random_state=seed).fit(x_tr, y_tr)
+        )
+    acc = [evaluate_accuracy(model, x_val, y_val) for model in models]
+    return models[np.argmax(acc)]
 
 
 def most_frequent_train(x_tr, y_tr):
@@ -79,20 +134,40 @@ def evaluate_accuracy(model, x, y):
 def main(args):
     # Read data
     if not args.train:
-        print("Train data file missing")
+        print("Train data file missing. Training will not proceed.")
         return
     if not args.test:
-        print("Test data file missing")
+        print("Test data file missing. Training will not proceed.")
         return
-    x_tr, x_tt, y_tr, y_tt = read_data(args.train, args.test, do_print=args.do_print)
+    x_tr, x_tt_orig, y_tr, y_tt_orig = read_data(args.train, args.test)
+    if args.do_print:
+        print(f"Number of features: {x_tr.shape[1]}")
+        print(f"Number of classes: {np.unique(y_tr).shape[0]}")
+    # Split
+    x_tr, x_val, x_tt, y_tr, y_val, y_tt = split(
+        x_tr, y_tr, args.pct_val, args.pct_test, seed=args.seed
+    )
+    if len(y_tt) == 0:
+        x_tt = x_tt_orig
+        y_tt = y_tt_orig
+    if args.do_print:
+        print(f"Train size: {len(x_tr)}")
+        print(f"Validation size: {len(x_val)}")
+        print(f"Test size: {len(x_tt)}")
 
     # Normalize
     if args.do_normalize:
-        x_tr, x_tt = normalize(x_tr, x_tt)
+        x_tr, x_val, x_tt = normalize(x_tr, x_val, x_tt)
 
     # Train models
     baseline = most_frequent_train(x_tr, y_tr)
-    logreg = logistic_regression_train(x_tr, y_tr, n_iters=500, seed=0)
+    if args.do_hp_validation and len(x_val) > 0:
+        c_values = 10.0 ** np.arange(-2, 3)
+        logreg = logistic_regression_validate_reg_train(
+            x_tr, y_tr, x_val, y_val, c_values, n_iters=500, seed=args.seed
+        )
+    else:
+        logreg = logistic_regression_train(x_tr, y_tr, n_iters=500, seed=args.seed)
 
     # Evaluation
     baseline_acc_tr = evaluate_accuracy(baseline, x_tr, y_tr)
